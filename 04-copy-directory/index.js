97b65bc531c45dec59d466b3f5de3ddd53a2c982
Egor Kukanov <balovenisydibbl@gmail.com>
2023-05-08 20:31:06 +0400

    feat: solution 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,29 @@
 
+const fs = require('fs');
+const path = require('path');
+const srcDir = path.join(__dirname, 'files');
+const destDir = path.join(__dirname, 'files-copy');
+
+
+copyDir = (srcDir, destDir) => {
+    if (!fs.existsSync(destDir)) {
+        fs.mkdirSync(destDir);
+    }
+
+    const files = fs.readdirSync(srcDir);
+
+    files.forEach((file) => {
+        const srcPath = path.join(srcDir, file);
+        const destPath = path.join(destDir, file);
+
+        if (fs.statSync(srcPath).isDirectory()) {
+            copyDir(srcPath, destPath);
+        } else {
+            fs.copyFileSync(srcPath, destPath);
+        }
+    });
+}
+
+copyDir(srcDir, destDir);
+
+console.log('Файлы успешно скопированы!');
